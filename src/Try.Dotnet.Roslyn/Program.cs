c61da77a080c42993e064862b9402a646ff46272
nikaburu <herman.starzhynski@hotmail.com>
2018-05-20 11:55:23 +0100

    initial project for roslyn refactoring help example

---
+++ src/Try.Dotnet.Roslyn/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Try.Dotnet.Roslyn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

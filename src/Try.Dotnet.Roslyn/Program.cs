b8e213f509f4a0f3bb135ec16a3d1b337ab2bb92
nikaburu <herman.starzhynski@hotmail.com>
2018-11-08 14:55:45 +0000

    Add basic program flow

--- src/Try.Dotnet.Roslyn/Program.cs
+++ src/Try.Dotnet.Roslyn/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.Extensions.Logging;
 using System;
+using System.IO;
 
 namespace Try.Dotnet.Roslyn
 {
@@ -6,7 +8,36 @@ namespace Try.Dotnet.Roslyn
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region settings
+            
+            string contractPath = "sources\\IPropertyServices.cs";
+            string servicePath = "sources\\PropertyServices.cs";
+
+            #endregion
+
+            ILoggerFactory loggerFactory = new LoggerFactory().AddConsole();
+            ILogger logger = loggerFactory.CreateLogger<Program>();
+
+            string contract = File.ReadAllText(contractPath);
+            logger.LogInformation(contract);
+
+            string service = File.ReadAllText(servicePath);
+            logger.LogInformation(service);
+
+            (contract, service) = Roslyn.Refactor(contract, service);
+
+            File.WriteAllText(contractPath, contract);
+            File.WriteAllText(servicePath, service);
+        }
+    }
+
+    class Roslyn
+    {
+        public static (string contract, string service) Refactor(string contract, string service)
+        {
+            //todo
+            //return (interfaceRoot.ToFullString(), root.NormalizeWhitespace().ToFullString());
+            return (contract, service);
         }
     }
 }
